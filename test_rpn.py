d11bc51df5b233fb3d8d39d14302c81852ae7360
aintner <waintner@umich.edu>
2017-03-11 14:11:02 -0500

    fixed test tabs issue

--- test_rpn.py
+++ test_rpn.py
@@ -7,9 +7,9 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-    def test_subtract(self):
+	def test_multiply(self):
 		result = rpn.calculate('5 3 *')
 		self.assertEqual(15, result)
-    def test_subtract(self):
+	def test_divide(self):
 		result = rpn.calculate('15 5 /')
 		self.assertEqual(3, result)
